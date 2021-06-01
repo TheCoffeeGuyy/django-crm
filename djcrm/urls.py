43fce393286b80a471891213dd85d7c74c121b70
TheCoffeeGuy <huuphuoc252513@gmail.com>
2021-06-01 20:28:14 +0700

    url namespace

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from leads.views import homePage
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', homePage),
+    path('leads/', include('leads.urls', namespace="leads"))
 ]
--- leads/views.py
+++ leads/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Lead
 
-def homePage(request):
+def leadList(request):
     leads = Lead.objects.all()
 
     context = {
         "leads": leads
     }
-    return render(request, "leads/homePage.html", context)
+    return render(request, "leads/leadList.html", context)
