bdbccb3d44f29b0af97f8016964005bea78442ff
TheCoffeeGuy <huuphuoc252513@gmail.com>
2021-06-02 20:35:41 +0700

    form handle

---
+++ leads/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import leadList,leadDetail, leadCreate
+app_name = "leads"
+
+urlpatterns = [
+    path('', leadList),
+    path('int:<pk>/', leadDetail),
+    path('create/', leadCreate),
+]
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm
 
 def leadList(request):
     leads = Lead.objects.all()
@@ -9,3 +10,25 @@ def leadList(request):
         "leads": leads
     }
     return render(request, "leads/leadList.html", context)
+
+def leadDetail(request, pk):
+    print(pk)
+    lead = Lead.objects.get(id=pk)
+    context = {'lead': lead}
+    return render(request, "leads/leadDetail.html", context)
+
+def leadCreate(request):
+    form = LeadForm()
+    if request.method == 'POST':
+        print('receving')
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print('form valid')
+            print(form.cleaned_data)
+            firstName = form.cleaned_data['firstName']
+            lastName = form.cleaned_data['lastName']
+            age = form.cleaned_data['age']
+    context = {
+        "form": LeadForm()
+    }
+    return render(request, "leads/leadCreate.html", context)
